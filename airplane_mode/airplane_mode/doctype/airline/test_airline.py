f37a48c0c7c8610740f2795ca686ff8294e9b5a2
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-12-17 18:38:51 +0300

    initial commit

---
+++ airplane_mode/airplane_mode/doctype/airline/test_airline.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Lucky Tsuma and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAirline(FrappeTestCase):
+	pass
