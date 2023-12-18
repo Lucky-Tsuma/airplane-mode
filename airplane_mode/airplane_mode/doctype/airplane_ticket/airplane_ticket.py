4ba58a465fecb7f51c70a1c1bcc13ffba14fe8d2
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-12-18 13:39:26 +0300

    feat: prevent ticket submission if status is not `Boarded`

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, Lucky Tsuma and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class AirplaneTicket(Document):
@@ -11,6 +11,10 @@ class AirplaneTicket(Document):
 	def before_save(self):
 		self.total_add_ons_amount = self.calculate_total_add_ons_amount()
 		self.calculate_total_amount()
+
+	def before_submit(self):
+		if self.status != 'Boarded':
+			frappe.throw('Cannot submit ticket. Passenger is not on board')
 
 	def calculate_total_add_ons_amount(self):
 		return sum(x.amount for x in self.add_ons) or 0
@@ -26,7 +30,7 @@ class AirplaneTicket(Document):
 			if x.item not in seen_add_on_types:
 				seen_add_on_types.add(x.item)
 				unique_add_on_types.append(x)
-				
+
 		self.add_ons = unique_add_on_types
 		
 
