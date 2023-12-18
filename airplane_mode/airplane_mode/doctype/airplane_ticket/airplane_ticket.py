d7d4ade4c8cc614e2c462b2b490a66ffc8c0bae4
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-12-18 13:31:11 +0300

    feat: remove duplicate add_on items

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -5,4 +5,28 @@
 from frappe.model.document import Document
 
 class AirplaneTicket(Document):
-	pass
+	def validate(self):
+		self.remove_duplicate_add_on_types()
+
+	def before_save(self):
+		self.total_add_ons_amount = self.calculate_total_add_ons_amount()
+		self.calculate_total_amount()
+
+	def calculate_total_add_ons_amount(self):
+		return sum(x.amount for x in self.add_ons) or 0
+
+	def calculate_total_amount(self):
+		self.total_amount = self.flight_price + self.total_add_ons_amount
+
+	def remove_duplicate_add_on_types(self):
+		unique_add_on_types = []
+		seen_add_on_types = set()
+
+		for x in self.add_ons:
+			if x.item not in seen_add_on_types:
+				seen_add_on_types.add(x.item)
+				unique_add_on_types.append(x)
+				
+		self.add_ons = unique_add_on_types
+		
+
