dd4b425e8cd1431b2560a8cd382aa84d2d8fb0fa
Lucky-Tsuma <tsumahmumba@gmail.com>
2024-01-21 17:26:57 +0300

    feat: assign seats via dialog

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,31 @@
 // Copyright (c) 2023, Lucky Tsuma and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Airplane Ticket', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Airplane Ticket", {
+    refresh: function (frm) {
+        frm.add_custom_button(
+            __("Assign Seat"),
+            function () {
+                let dialog = new frappe.ui.Dialog({
+                    title: "Select Seat",
+                    fields: [
+                        {
+                            label: "Seat Number",
+                            fieldname: "seat_number",
+                            fieldtype: "Data",
+                        },
+                    ],
+                    size: "small",
+                    primary_action_label: "Assign",
+                    primary_action(values) {
+                        frm.set_value("seat", values.seat_number);
+                        frm.save();
+                        dialog.hide();
+                    },
+                });
+                dialog.show();
+            },
+            __("Actions")
+        );
+    },
 });
