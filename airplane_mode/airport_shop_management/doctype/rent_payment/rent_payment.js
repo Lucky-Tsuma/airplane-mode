8b9640fbf2eb17f056153906f4e557878201c057
Lucky-Tsuma <tsumahmumba@gmail.com>
2024-01-25 16:23:11 +0300

    feat: add rent payment receipt print format

--- airplane_mode/airport_shop_management/doctype/rent_payment/rent_payment.js
+++ airplane_mode/airport_shop_management/doctype/rent_payment/rent_payment.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2024, Lucky Tsuma and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Rent Payment", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Rent Payment", {
+    onload: function (frm) {
+        if (frm.doc.__islocal && (!frm.doc.from_date || !frm.doc.to_date)) {
+            frm.set_value("from_date", frappe.datetime.add_months(frappe.datetime.get_today(), -1));
+            frm.set_value("to_date", frappe.datetime.get_today());
+        }
+    },
+});
