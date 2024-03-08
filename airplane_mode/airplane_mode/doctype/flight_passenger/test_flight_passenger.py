e4e675f629ed5bef7c3451f80c07feaa36d3fc4b
Lucky Tsuma <tsuma@Luckys-MacBook-Air.local>
2024-03-08 16:28:00 +0300

    feat: add tests for flight passenger doctype

--- airplane_mode/airplane_mode/doctype/flight_passenger/test_flight_passenger.py
+++ airplane_mode/airplane_mode/doctype/flight_passenger/test_flight_passenger.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2023, Lucky Tsuma and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestFlightPassenger(FrappeTestCase):
-	pass
+	def test_set_fullname_is_set_automatically(self):
+		test_passenger = frappe.get_doc({
+			"doctype": "Flight Passenger",
+			"first_name": "John",
+			"last_name": "Doe",
+			"date_of_birth": "2000-12-12"
+		}).insert()
+
+		self.assertEqual(test_passenger.full_name, "John Doe")
+
+	def test_set_firstname_only_if_lastname_not_set(self):
+		test_passenger = frappe.get_doc({
+			"doctype": "Flight Passenger",
+			"first_name": "Jane",
+			"last_name": "",
+			"date_of_birth": "2000-12-12"
+		}).insert()
+
+		self.assertEqual(test_passenger.full_name, "Jane")
