7b5928428e749071bb3d4bfb7ae958ab2288207d
Lucky-Tsuma <tsumahmumba@gmail.com>
2024-05-04 16:08:13 +0300

    chore: format code with prettier

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
@@ -8,21 +8,23 @@ frappe.ui.form.on("Airplane Flight", {
         });
     },
     onload: function (frm) {
-        frm.set_query('departure_gate', () => {
+        frm.set_query("departure_gate", () => {
             return {
                 filters: {
-                    'airport': frm.doc.source_airport
-                }
-            }
-        })
-        
+                    airport: frm.doc.source_airport,
+                },
+            };
+        });
+
         if (!frm.doc.__unsaved) {
-            frappe.call({
-                doc: frm.doc,
-                method: "show_remaining_seats",
-            }).then((r) => {
-                frm.set_intro(`${r["message"]} seats available`);
-            })
+            frappe
+                .call({
+                    doc: frm.doc,
+                    method: "show_remaining_seats",
+                })
+                .then((r) => {
+                    frm.set_intro(`${r["message"]} seats available`);
+                });
         }
     },
 });
--- airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.js
+++ airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.js
@@ -1,8 +1,7 @@
 // Copyright (c) 2023, Lucky Tsuma and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Flight Passenger', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Flight Passenger", {
+    // refresh: function(frm) {
+    // }
 });
